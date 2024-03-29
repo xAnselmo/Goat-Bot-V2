3989f754279d5574aba462505dc009f3838e49de
xAnselmo <164136289+xAnselmo@users.noreply.github.com>
2024-03-30 02:43:34 +0800

    Update remini.js

--- scripts/cmds/remini.js
+++ scripts/cmds/remini.js
@@ -25,27 +25,27 @@ module.exports = {
         imageUrl = replyAttachment.url;
       } else {
         return api.sendMessage(
-          { body: "⚠️ | 𝗠𝗨𝗦𝗧 𝗥𝗘𝗣𝗟𝗬 𝗧𝗢 𝗔𝗡 𝗜𝗠𝗔𝗚𝗘." },
+          { body: "┐(￣ヘ￣)┌ | Must reply to an image." },
           event.threadID
         );
       }
     } else if (args[0]?.match(/(https?:\/\/.*\.(?:png|jpg|jpeg))/g)) {
       imageUrl = args[0];
     } else {
-      return api.sendMessage({ body: "☣️ | 𝗠𝗨𝗦𝗧 𝗥𝗘𝗣𝗟𝗬 𝗧𝗢 𝗔𝗡 𝗜𝗠𝗔𝗚𝗘 𝗢𝗥 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗔𝗡 𝗜𝗠𝗔𝗚𝗘 𝗨𝗥𝗟." }, event.threadID);
+      return api.sendMessage({ body: "┐(￣ヘ￣)┌ | Must reply to an image or provide an image URL." }, event.threadID);
     }
 
     try {
       const url = await tinyurl.shorten(imageUrl);
       const k = await a.get(`https://www.api.vyturex.com/upscale?imageUrl=${url}`);
 
-      message.reply(📲📳) | 𝗣𝗟𝗘𝗔𝗦𝗘 𝗪𝗔𝗜𝗧...");
+      message.reply("⊂(・﹏・⊂) | Please wait...");
 
       const resultUrl = k.data.resultUrl;
 
-      message.reply({ body: "✅✅ | 𝗜𝗠𝗔𝗚𝗘 𝗘𝗡𝗛𝗔𝗡𝗖𝗘𝗗 𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬.", attachment: await global.utils.getStreamFromURL(resultUrl) });
+      message.reply({ body: "<(￣︶￣)> | Image Enhanced.", attachment: await global.utils.getStreamFromURL(resultUrl) });
     } catch (error) {
-      message.reply("😭😭 | 𝗔𝗣𝗜 𝗗𝗘𝗔𝗗...");
+      message.reply("(┌・。・)┌ | Api Dead...");
     }
   }
 };
