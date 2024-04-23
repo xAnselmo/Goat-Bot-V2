e027d4491baa8b2d086bcc633ec28a1d8cc1d1c9
xAnselmo <164136289+xAnselmo@users.noreply.github.com>
2024-04-23 09:03:18 +0800

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -31,7 +31,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Please enter the message you want to send to all groups",
-			notification: "Notification from admin bot to all chat groups (do not reply to this message)",
+			notification: "➪𝖭𝖮𝖳𝖨𝖥𝖨𝖢𝖠𝖳𝖨𝖮𝖭 𝖥𝖱𝖮𝖬 𝖠𝖣𝖬𝖨𝖭 𝖳𝖮 𝖠𝖫𝖫 𝖦𝖢 𝖠𝖭𝖣 𝖴𝖲𝖤𝖱♡"
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %1 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
